correct the data normalization strategy

backbone/TimesNet/model.py
@@ -59,8 +59,6 @@ class TimesNet(nn.Module):
         pred = dec_out[:, -self.pred_len:, :]
 
         if self.flag == 'Plugin':
-            map1 = pred.clone()
-            pred, reco, map2 = self.plugin(x_enc_copy, x_mark_enc_copy, map1, x_mark_dec_copy[:, -self.pred_len:, :])
-            return pred, reco, map1, map2
-        else:
-            return pred, None, None, None
+            pred = self.plugin(x_enc_copy, x_mark_enc_copy, pred, x_mark_dec_copy[:, -self.pred_len:, :])
+
+        return pred

main.py
@@ -1,7 +1,6 @@
 import argparse
 from time import time
 
-import numpy as np
 import torch
 
 from solver.solver import Solver
@@ -25,11 +24,11 @@ if __name__ == '__main__':
     parser.add_argument('--pred_len', type=int, default=192, help='length of prediction window')
 
     """ Plugin """
-    parser.add_argument('--dim', type=int, default=512, help='dimension of hidden state')
-    parser.add_argument('--dff', type=int, default=2048, help='dimension of feed forward')
+    parser.add_argument('--dim', type=int, default=256, help='dimension of hidden state')
+    parser.add_argument('--dff', type=int, default=512, help='dimension of feed forward')
     parser.add_argument('--head_num', type=int, default=8, help='number of heads')
     parser.add_argument('--layer_num', type=int, default=2, help='number of layers')
-    parser.add_argument('--dropout', type=float, default=0.1, help='dropout rate')
+    parser.add_argument('--dropout', type=float, default=0.6, help='dropout rate')
     parser.add_argument('--q', type=float, default=0.75, help='quantile')
 
     """ Optim """
@@ -53,7 +52,6 @@ if __name__ == '__main__':
 
     fixSeed(args.seed)
 
-    mse_list, mae_list = [], []
     for ii in range(args.itr):
         setting = '{0}_{1}_{2}_{3}_{4}_{5}'.format(
             args.model,
@@ -76,22 +74,18 @@ if __name__ == '__main__':
 
         print('\n>>>>>>>>  testing : {}  <<<<<<<<\n'.format(setting))
         start = time()
-        mse, mae, model_size = solver.test()
-        mse_list.append(mse)
-        mae_list.append(mae)
+        res = solver.test()
         test_time = time() - start
         print('Testing Time: {:.4f}s'.format(test_time))
 
         f = open('./result.txt', 'a')
         f.write(setting + "  \n")
         if not args.only_test:
-            f.write('Train:{0:.4f} s, Test:{1:.4f} s, Size:{2:.4f} MB\n'.format(train_time, test_time, model_size))
-        f.write('MSE:{0:.4f}, MAE:{1:.4f}\n'.format(mse, mae))
+            f.write('Train:{0:.4f} s, Test:{1:.4f} s, Size:{2:.4f} MB\n'.format(train_time, test_time, res['size']))
+        f.write('MSE:{0:.4f}, MAE:{1:.4f}\n'.format(res['MSE'], res['MAE']))
         f.write('\n')
         f.close()
 
         torch.cuda.empty_cache()
 
-    print('Done!')
-    print('MSE: {:.4f} +- {:.4f}'.format(np.mean(mse_list), np.std(mse_list)))
-    print('MAE: {:.4f} +- {:.4f}'.format(np.mean(mae_list), np.std(mae_list)))
+    print('\nDone!')

plugin/Plugin/model.py
@@ -37,17 +37,24 @@ class Plugin(nn.Module):
         )
 
     def forward(self, x_enc_true, x_mark_enc, x_dec_pred, x_mark_dec):
+        means = torch.mean(x_enc_true, dim=1, keepdim=True)
+        stdev = torch.std(x_enc_true, dim=1, keepdim=True) + 1e-6
+        x_enc_true = (x_enc_true - means) / stdev
+        x_dec_pred = (x_dec_pred - means) / stdev
+
         # map
         x_enc_map = self.Encoder(x_mark_enc)
         x_dec_map = self.Encoder(x_mark_dec)
 
         # denormalize
-        scale_true = torch.quantile(x_enc_true, self.q, 1, True) - torch.quantile(x_enc_true, 1 - self.q, 1, True)
-        scale_map = torch.quantile(x_enc_map, self.q, 1, True) - torch.quantile(x_enc_map, 1 - self.q, 1, True)
-        stdev = scale_true / scale_map
-        means = torch.median(x_enc_true - x_enc_map * stdev, dim=1, keepdim=True)[0]
-        x_enc_map = x_enc_map * stdev + means
-        x_dec_map = x_dec_map * stdev + means
+        robust_means_true = torch.median(x_enc_true, dim=1, keepdim=True)[0]
+        robust_means_map = torch.median(x_enc_map, dim=1, keepdim=True)[0]
+        robust_stdev_true = torch.quantile(x_enc_true, self.q, 1, True) - torch.quantile(
+            x_enc_true, 1 - self.q, 1, True) + 1e-6
+        robust_stdev_map = torch.quantile(x_enc_map, self.q, 1, True) - torch.quantile(
+            x_enc_map, 1 - self.q, 1, True) + 1e-6
+        x_enc_map = (x_enc_map - robust_means_map) / robust_stdev_map * robust_stdev_true + robust_means_true
+        x_dec_map = (x_dec_map - robust_means_map) / robust_stdev_map * robust_stdev_true + robust_means_true
 
         # combine
         error = x_enc_true - x_enc_map
@@ -55,4 +62,6 @@ class Plugin(nn.Module):
         x_dec = torch.stack([x_dec_map, x_dec_pred], dim=-1)
         pred = torch.sum(x_dec * weight, dim=-1)
 
-        return pred, x_enc_map, x_dec_map
+        pred = pred * stdev + means
+        
+        return pred

solver/solver.py
@@ -83,12 +83,8 @@ class Solver:
         x_dec = torch.concat([x_data[:, -label_len:, :], torch.zeros_like(y_data)], dim=1).to(self.device)
         x_mark_dec = torch.concat([x_time[:, -label_len:, :], y_time], dim=1).to(self.device)
 
-        pred, reco, map1, map2 = self.model(x_enc, x_mark_enc, x_dec, x_mark_dec)
-        if self.args.flag == 'Plugin':
-            loss = self.criterion(torch.concat([pred, reco, map1, map2], dim=1),
-                                  torch.concat([y_data, x_data, y_data, y_data], dim=1))
-        else:
-            loss = self.criterion(pred, y_data)
+        pred = self.model(x_enc, x_mark_enc, x_dec, x_mark_dec)
+        loss = self.criterion(pred, y_data)
 
         return loss, pred
 
@@ -144,11 +140,12 @@ class Solver:
         pred = np.concatenate(pred, axis=0)
         true = np.concatenate(true, axis=0)
 
-        print('Hist Shape:', hist.shape)
+        print('\nHist Shape:', hist.shape)
         print('Pred Shape:', pred.shape)
         print('True Shape:', true.shape)
 
-        mse, mae = evaluate(pred, true)
-        print('MSE:{0:.4f}, MAE:{1:.4f}'.format(mse, mae))
+        res = evaluate(pred, true)
+        res["Size"] = model_size
+        print('\nMSE:{0:.4f}, MAE:{1:.4f}'.format(res['MSE'], res['MAE']))
 
-        return mse, mae, model_size
+        return res

util/eval.py
@@ -1,11 +1,17 @@
-import numpy as np
+from einops import rearrange
+from sklearn.metrics import mean_squared_error, mean_absolute_error
 
 
 def evaluate(pred, true):
-    mse = np.mean(np.square(pred - true))
-    mae = np.mean(np.abs(pred - true))
+    pred = rearrange(pred, 'N L C -> (N C) L')
+    true = rearrange(true, 'N L C -> (N C) L')
 
-    return mse, mae
+    res = {
+        'MSE': mean_squared_error(true, pred),
+        'MAE': mean_absolute_error(true, pred),
+    }
+
+    return res
 
 
 def getModelSize(model):